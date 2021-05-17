124f86b2d5e94f0272058b2985dd1958dd564974
CodeByMini <caspian@lightsbymini.se>
2021-05-17 09:52:58 +0200

    added message class variable date

--- src/main/java/com/example/javarest/Models/Message.java
+++ src/main/java/com/example/javarest/Models/Message.java
@@ -3,6 +3,7 @@ package com.example.javarest.Models;
 public class Message {
     private String temp;
     private String hum;
+    private String date;
 
 
     public Message(String temp, String hum){
@@ -24,5 +25,13 @@ public class Message {
 
     public void setHum(String hum) {
         this.hum = hum;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
     }
 }
